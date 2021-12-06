8b733d86778bde6761f3250354dd98a62ff41221
noxannan <noxannan@gmail.com>
2021-12-06 07:39:16 +0300

    Implement sub function

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,13 @@ public class HelloWorld {
         int a=5;
         int b=3;
         System.out.println(add(a,b));
+        System.out.println(sub(a,b));
     }
     private static int add(int a,int b){
         return a+b;
+    }
+
+    private static int sub(int a,int b){
+        return a-b;
     }
 }
