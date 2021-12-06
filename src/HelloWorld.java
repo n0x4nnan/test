d68dd3b4383ee5059521481a53e59e55d3c56b25
noxannan <noxannan@gmail.com>
2021-12-06 07:37:59 +0300

    third commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
-        System.out.println("Hello world!");
+        int a=5;
+        int b=3;
+        System.out.println(add(a,b));
+    }
+    private static int add(int a,int b){
+        return a+b;
     }
 }
