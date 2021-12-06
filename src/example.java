4e31b963ad19960e2069997a2973ebe4ce084288
noxannan <noxannan@gmail.com>
2021-12-06 18:08:50 +0300

    First commit aboud tictactoe game

---
+++ src/example.java
@@ -0,0 +1,11 @@
+public class example {
+    public static void main(String[] args) {
+        System.out.println("Use the following mapping table to specify a cell using numbers from 1 to 9:");
+
+        char[][] gameTable = {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+        };
+    }
+}
