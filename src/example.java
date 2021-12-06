adfe37a5ab72c7bf71d83e440448ef0079736709
noxannan <noxannan@gmail.com>
2021-12-06 18:12:28 +0300

    Implement printGameTable function

--- src/example.java
+++ src/example.java
@@ -8,4 +8,15 @@ public class example {
                 {' ', ' ', ' '}
         };
     }
+
+    private static void printGameTable(char[][] gameTable) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + gameTable[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("-------------");
+    }
 }
