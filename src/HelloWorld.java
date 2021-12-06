1a567fcd42b4ea1967911a61aad174c9ddf0ec17
noxannan <noxannan@gmail.com>
2021-12-06 07:57:45 +0300

    Reformated code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,29 +1,33 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int a=5;
-        int b=3;
-        System.out.println(add(a,b));
-        System.out.println(sub(a,b));
-        System.out.println(mul(a,b));
-        System.out.println(div(a,b));
-        System.out.println(mod(a,b));
-    }
-    private static int add(int a,int b){
-        return a+b;
+        int a = 5;
+        int b = 3;
+        System.out.println(add(a, b));
+        System.out.println(sub(a, b));
+        System.out.println(mul(a, b));
+        System.out.println(div(a, b));
+        System.out.println(mod(a, b));
     }
 
-    private static int sub(int a,int b){
-        return a-b;
+    private static int add(int a, int b) {
+        return a + b;
     }
-    private static int mul(int a,int b){
-        return a*b;
-    }
-    private static int div(int a,int b){
-        return a/b;
-    }
-private static int mod(int a,int b){
-        return a%b;
 
-}
+    private static int sub(int a, int b) {
+        return a - b;
+    }
+
+    private static int mul(int a, int b) {
+        return a * b;
+    }
+
+    private static int div(int a, int b) {
+        return a / b;
+    }
+
+    private static int mod(int a, int b) {
+        return a % b;
+
+    }
 
 }
